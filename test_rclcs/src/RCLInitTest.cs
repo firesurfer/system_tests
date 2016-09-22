25c6c9b044846ae16b2039ac25726f8019c4efb8
firesurfer <firesurfer65@yahoo.de>
2016-09-22 18:12:36 +0200

    Added a not working test

---
+++ test_rclcs/src/RCLInitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework.Constraints;
+using NUnit.Framework;
+
+using rclcs;
+namespace test_rclcs
+{
+	[TestFixture]
+	public class RCLInitTest:AssertionHelper
+	{
+		[Test]
+		public void CreateAndDelete()
+		{
+			using (RCL rcl = new RCL ()) 
+			{
+				Assert.IsNotNull (rcl);
+				rcl.Init (new string[]{ });
+
+				Action<string[]> init_delegate = new Action<string[]> (rcl.Init);
+				Assert.DoesNotThrow (() => init_delegate(new string[]{}));
+				Assert.Throws<ArgumentNullException> (() => init_delegate (null));
+			}
+		
+		}
+		[Test]
+		public void CreateAndDeleteManual()
+		{
+
+			RCL rcl = new RCL ();
+			Assert.IsNotNull (rcl);
+
+			Action<string[]> init_delegate = new Action<string[]> (rcl.Init);
+			Assert.DoesNotThrow (() => init_delegate(new string[]{}));
+			Assert.Throws<ArgumentNullException> (() => init_delegate (null));
+
+			rcl.Dispose ();
+
+		}
+	}
+}
+
